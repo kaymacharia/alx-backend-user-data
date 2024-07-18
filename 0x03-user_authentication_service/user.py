e69d5f3b1bd2696a9da2f25f40edafa9b55450c7
kaymacharia <kaymachariaa@gmail.com>
2024-07-18 23:06:10 +0300

    new

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+SQLAlchemy model named User for a database table named users
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''
+    SQLAlchemy model for User
+    '''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
